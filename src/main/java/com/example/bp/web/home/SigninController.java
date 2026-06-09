86a12d227e2838312d307d4b1f705140af78d8dd
ukhyeonPark-mig <portal_se1@mig.kr>
2026-06-09 15:21:59 +0900

    AsyncConfig 비동기 설정 클래스 제거
    어플리케이션 시작점에 설정하는것으로 대체,

--- src/main/java/com/example/bp/web/home/SigninController.java
+++ src/main/java/com/example/bp/web/home/SigninController.java
@@ -21,6 +21,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
+
+
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
